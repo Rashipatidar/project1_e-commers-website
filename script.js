f77a166403644dec8200158a9c9d2d31d1beff84
Rashi Patidar <rashipatidar401@gmail.com>
2024-09-12 14:01:32 +0530

    final style and upadate is done

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.getElementById('loginForm').addEventListener('submit',function(event) {
     Event.preventDefault();
+
+   
+        document.getElementById('loginform').addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent default form submission
+
+            // You can add your form validation and AJAX login request here
+
+            // Redirect to home page upon successful login
+            window.location.href = 'home.html'; // Adjust path as needed
+        });
     
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
